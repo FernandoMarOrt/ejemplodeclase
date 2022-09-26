9845c53fac8dae81382e719952d2a7c4a369f8a9
FernandoMarOrt <fernandomartinezortiz003@gmail.com>
2022-09-26 13:48:43 +0200

    Añade el nombre al system.out

--- src/main/java/fernando/EJEMPLOGIT.java
+++ src/main/java/fernando/EJEMPLOGIT.java
@@ -12,6 +12,6 @@ package fernando;
 public class EJEMPLOGIT {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("FERNANDO");
     }
 }
